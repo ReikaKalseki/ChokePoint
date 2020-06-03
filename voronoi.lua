fc14677b505b10e8e6a06532f5ca2d48ab34e2f7
Reika <reikasminecraft@gmail.com>
2020-06-03 11:16:55 -0400

    Update 13

--- voronoi.lua
+++ voronoi.lua
@@ -1,61 +1,54 @@
---[[
-Ported from libnoise. As usual with noise code I have incorporated, '--' comments are theirs.
---]]
+local VORONOI_MAX_POINTS_PER_REGION = 2
+local VORONOI_POINTS_RESOLUTION = 1024
 
-VoronoiNoise = {}
+    local function hash(x)
+        local function lsl_xor(val)
+            local tmp = bit32.rshift(val, 16)
+            return bit32.bxor(tmp, val)
+        end
 
-local X_NOISE_GEN = 1
-local Y_NOISE_GEN = 31337
---local Z_NOISE_GEN = 263
-local SEED_NOISE_GEN = 1013
+        local magic = math.floor(0x45d9f3b)
 
-local SQRT_3 = math.sqrt(3)
+        x = lsl_xor(x) * magic
+        x = bit32.band(x, 0xffffffff)
+        x = lsl_xor(x) * magic
+        x = bit32.band(x, 0xffffffff)
+        x = lsl_xor(x)
+        x = bit32.band(x, 0xffffffff)
 
-local function IntValueNoise3D (x, y, seed)
-  -- All constants are primes and must remain prime in order for this noise function to work correctly.
-  local n = (
-  X_NOISE_GEN    * x
-  + Y_NOISE_GEN    * y
-  --+ Z_NOISE_GEN    * z
-  + SEED_NOISE_GEN * seed)
+        return math.floor(x)
+    end
 
-  n = bit32.band(n, 0x7fffffff)
-
-  n = bit32.bxor(bit32.rshift(n, 13), n)
-
-  local a = (n * (n * n * 60493 + 19990303) + 1376312589)
-
-  return bit32.band(a, 0x7fffffff)
-end
-
-local function ValueNoise3D (x, y, seed)
-  return 1.0 - (IntValueNoise3D (x, y, seed) / 1073741824.0)
-end
+    local function hash_xy(x, y)
+        local h = hash(x)
+        h = hash(bit32.bxor(h, y))
+        return h
+    end
 
 function GetValue(x, y, seed)
-  local xInt = math.floor(x)
-  local yInt = math.floor(y)
+  local h = hash_xy(x, y)
+  local result_count = 1 + h % (VORONOI_MAX_POINTS_PER_REGION - 1)
   
   local points = {}
 
-  -- Inside each unit cube, there is a seed point at a random position.  Go through each of the nearby cubes until we find a cube with a seed point that is closest to the specified position.
-  for dy = -2,2 do
-    for dx = -2,2 do
-
-      -- Calculate the position and distance to the seed point inside of this unit cube.
-      local xCur = dx+xInt
-      local yCur = dy+yInt
-      local xPos = xCur + ValueNoise3D (xCur, yCur, seed)
-      local yPos = yCur + ValueNoise3D (xCur, yCur, seed+1)
-      local xDist = xPos - x
-      local yDist = yPos - y
+  
+  for i=0,result_count do
+            h = hash(h)
+            local rx = (h % VORONOI_POINTS_RESOLUTION) / VORONOI_POINTS_RESOLUTION
+            h = hash(h)
+            local ry = (h % VORONOI_POINTS_RESOLUTION) / VORONOI_POINTS_RESOLUTION
+			
+			
+      local xDist = rx - x
+      local yDist = ry - y
       local dist = xDist * xDist + yDist * yDist
 
-      table.insert(points, {src = {xCur, yCur}, result = {xPos, yPos}, dist = dist})
-    end
-  end
+      table.insert(points, {src = {0, 0}, result = {rx, ry}, dist = dist})
+        end
   
   table.sort(points, function(point1, point2) return point1.dist < point2.dist end)
+  
+  if true then return points[1].dist end
   
   local candidate1 = points[1].result
   local candidate2 = points[2].result
@@ -75,5 +68,3 @@ function GetValue(x, y, seed)
   
   return dist
 end
-
-return VoronoiNoise
