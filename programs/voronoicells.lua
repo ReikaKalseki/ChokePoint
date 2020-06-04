90fbc3094a2b51788c96c7a1197b84195c21e3ae
Reika <reikasminecraft@gmail.com>
2020-06-03 23:27:13 -0400

    Update 19

--- programs/voronoicells.lua
+++ programs/voronoicells.lua
@@ -4,8 +4,8 @@ require "config"
 
 --returns: 0 for land, 1 for normal water, 2 for deep water, 3 for normal green water, 4 for deep green water, 5 for cliff, 6 for "water-shallow" which is traversible
 function runTile(dx, dy)
-	dx = dx/2.5*Config.terrainScale
-	dy = dy/2.5*Config.terrainScale
+	dx = dx*Config.terrainScale
+	dy = dy*Config.terrainScale
 	local f0 = 0.0035*3
 	local f0b = 0.024*3
 	local f = 0.007
@@ -19,16 +19,17 @@ function runTile(dx, dy)
 	local wobble = 0.4*(SimplexNoise.Noise2D(dx*f0, dy*f0)+0.06*SimplexNoise.Noise2D(dx*f0b, dy*f0b))
 	local noise = GetValue(dx*f+wobble, dy*f+wobble)
 	local simplexBridges = SimplexNoise.Noise2D(dx*f2, dy*f2)+0.25*SimplexNoise.Noise2D(dx2*f3, dy2*f3)
-	simplexBridges = simplexBridges/6+0.14
+	simplexBridges = simplexBridges/6+0.095
 	local simplexOffset = SimplexNoise.Noise2D(dx3*f4, dy3*f4)
 	noise = noise+simplexOffset*0.03
 	local spawnZone = math.max(0, 1-(dx*dx+dy*dy)*0.02)
 	local baseNoise = noise+spawnZone*0.3
-	local thresh = 0.05*Config.riverSize
+	local thresh = 0.165*Config.riverSize
 	local value = baseNoise+math.max(simplexBridges, 0)*1
-	if value < thresh then
+	local shallows = 0.035
+	if value < thresh-shallows then
 		return 1
-	elseif value < thresh+0.025 then
+	elseif value < thresh+shallows then
 		return 6
 	else
 		return 0
